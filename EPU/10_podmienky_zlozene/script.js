742d3b9c5c2d7d417cf088c7daedae25ac641987
ZuzanaKarakova <zuzana.karakova@piaristi-pd.sk>
2026-05-18 13:19:46 +0200

    zlozene podmienky asi

---
+++ EPU/10_podmienky_zlozene/script.js
@@ -0,0 +1,50 @@
+/*let teplota = 19;
+
+if(teplota < 0){
+    console.log(`Pri teplote ${teplota}°C mrzne.`)
+}
+else if(teplota < 13) {
+    console.log(`Pri teplote ${teplota}°C je chladno.`)
+}
+else if(teplota < 25) {
+    console.log(`Pri teplote ${teplota}°C je príjemne.`)
+}
+else {
+    console.log(`Pri teplote ${teplota}°C je horúco.`)
+}*/
+
+
+
+/*let vek = 16;
+
+if(vek < 3){
+    console.log("Batoľa");
+}
+else if(vek < 12){
+    console.log("Dieťa");
+}
+else if(vek < 18){
+    console.log("Teenager");
+}
+else if(vek < 65){
+    console.log("Dospelý");
+}
+else {
+    console.log("Dôchodca");
+}*/
+
+
+/*let vek = 19;
+let ucetVApke = true;
+
+if(vek > 18 && ucetVApke === true){
+    console.log(`Vstup do Jednoty povolený`)
+}*/
+
+
+let student = false;
+let dochodca = true;
+
+if (student || dochodca){
+    console.log(Vlaky zdarma.);
+}
